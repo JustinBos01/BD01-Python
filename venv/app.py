6c59a38423a2d05050b56b5e92e7c24b5d24e6d6
JustinBos01 <59139152+JustinBos01@users.noreply.github.com>
2021-10-07 11:03:21 +0200

    Web management finished

--- venv/app.py
+++ venv/app.py
@@ -1,17 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_modals import Modal
-import Include
 import requests
+import sys
+#sys.path.append('C:/Users/31638/Documents/GitHub/BD01-Python/venv/Include/Sphero_Bolt_Multiplatform_Python_Bleak-master/sphero_bolt.py')
+# import asyncio
+# import math
+# from typing import Dict
+# from sphero_bolt import SpheroBolt
+# from roll_with_input import *
 
-from requests import api
-#import scanner, get_tcp_adapter
-#from spherov2 import scanner
-#from spherov2.adapter.tcp_adapter import get_tcp_adapter
-#from spherov2.sphero_edu import SpheroEduAPI
-#
-#
-#with scanner.find_toy(adapter=get_tcp_adapter('localhost')) as toy:
-#    ...
 
 app =  Flask(__name__)
 modal = Modal(app)
@@ -19,6 +16,17 @@ api_url = "http://localhost:8080"
 
 @app.route('/')
 def index():
+#     distance = 1.5
+#     angle = 90
+#     collection = [{
+#         "distance":  1.5,
+#         "heading": 90
+#     }]
+#     collection.append({
+#         "distance": 1.5,
+#         "heading": 180
+#     })
+#     loop.run_until_complete(calculate_distance(my_sphero, collection))
     return workspaces()
 
 @app.route('/workspaces', methods=['GET', 'POST'])
@@ -31,11 +39,51 @@ def workspaces():
 def create_workspace(workspace_name):
     json_string = {"workspace_name": workspace_name}
     response = requests.get(api_url+"/workspaces/insert", json=json_string)
-    return workspaces()
+    return redirect('/')
 
-@app.route('/edit', methods=['GET', 'POST'])
-def edit():
-    return render_template("edit-environment.html")
+@app.route('/workspaces/delete/<workspace_id>', methods=['GET', 'POST'])
+def delete_workspace(workspace_id):
+    json_string = {"id": int(workspace_id)}
+    response = requests.delete(api_url+"/workspaces/delete/workspace_by_id", json=json_string)
+    return redirect('/')
+
+@app.route('/workspaces/update/<workspace_id>/<workspace_name>', methods=['GET', 'POST'])
+def update_workspace(workspace_id, workspace_name):
+    json_string = {"id": int(workspace_id), "workspace_name": workspace_name}
+    response = requests.put(api_url+"/workspaces/update", json=json_string)
+    return redirect('/')
+
+@app.route('/locations/<workspace_id>', methods=['GET', 'POST'])
+def location(workspace_id):
+    json_string = { 'workspace_id': int(workspace_id) }
+    response = requests.get(api_url+"/locations/by_workspace_id", json=json_string)
+    json_string = response.json()
+    return render_template("edit-environment.html", locations = json_string)
+
+@app.route('/locations/create/<workspace_id>/<angle>/<distance>', methods=['GET', 'POST'])
+def create_location(workspace_id, angle, distance):
+    json_string = { 'workspace_id': int(workspace_id), 'angle': int(angle), 'distance': int(distance) }
+    requests.post(api_url+"/locations/insert", json=json_string)
+    return redirect('/locations/' + workspace_id)
+
+@app.route('/locations/delete/<id>/<workspace_id>', methods=['GET', 'POST'])
+def delete_location(id, workspace_id):
+    json_string = { 'id': int(id) }
+    requests.delete(api_url+"/locations/delete/location_by_id", json=json_string)
+    return redirect('/locations/' + workspace_id)
+
+@app.route('/locations/update/<id>/<workspace_id>/<angle>/<distance>', methods=['GET', 'POST'])
+def update_location(id, workspace_id, angle, distance):
+    json_string = { 'id': int(id), 'angle': int(angle), 'distance': int(distance)}
+    requests.put(api_url+"/locations/update", json=json_string)
+    return redirect('/locations/' + workspace_id)
+
+#@app.route('/locations/<workspace_id>/<angle>/<distance>', methods=['GET', 'POST'])
+#def location_by_workspace_id(workspace_id, angle, distance):
+#    json_string = { 'workspace_id': int(workspace_id) }
+#    response = requests.get(api_url+"/locations/by_workspace_id", json=json_string)
+#    json_string = response.json()
+#    return render_template("edit-environment.html", locations = json_string, angle=angle, distance=distance)
 
 @app.route('/users', methods=['GET', 'POST'])
 def users():
